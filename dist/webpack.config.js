3e8f60198c601e50e36e162b66cce8f41cba04ec
yokoyama0927 <curashiko.927@icloud.com>
2023-12-16 10:17:19 +0900

    first commit

---
+++ dist/webpack.config.js
@@ -0,0 +1,29 @@
+"use strict";
+var path = require("path");
+module.exports = {
+    entry: {
+        bundle: "./src/index.ts",
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].js",
+        publicPath: "/dist/",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+            },
+        ],
+    },
+};
